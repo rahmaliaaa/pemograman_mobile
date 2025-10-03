26cafe1f0d1736de02ecf07b046ce7db92672ce4
rahmaliaaa <mozachikaa@gmail.com>
2025-10-03 23:21:37 +0700

    menambahkan drawer dan bottom navigation bar

--- flutter/flutter_application_3/lib/main.dart
+++ flutter/flutter_application_3/lib/main.dart
@@ -25,6 +25,46 @@ class LiaaApp extends StatelessWidget {
           onPressed: () {},
           child: Icon(Icons.add),
         ),
+        drawer: Drawer( // Menambahkan drawer
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const [
+              DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue,),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(
+                    color: Colors.white,fontSize: 24,),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.message),title: Text('Messages'),
+              ),
+              ListTile(
+                leading: Icon(Icons.account_circle),title: Text('Profile'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),title: Text('Settings'),
+              ),
+            ], 
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar( // Menambahkan bottom navigation bar
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+          ],
+        ),
       ),
     );
   }
