48f413f8c3d4de53322d0ad1e86961d7a8161bb8
rahmaliaaa <mozachikaa@gmail.com>
2025-10-03 10:39:55 +0700

    daftarnamamhs(tugas), intersection dan union, simpan barang, closure

---
+++ flutter/flutter_application_1/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LuqmanApp());
+}
+
+class LuqmanApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Lia App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Balonku ada 5",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontFamily: 'Arial',
+            ),
+          ),
+          backgroundColor: Colors.blue,
+          centerTitle: false,
+          foregroundColor: Colors.white,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Balonku ada 5\nRupa-rupa warnanya\nHijau, kuning, kelabu\nMerah muda dan biru",
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontFamily: 'Arial',
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Meletus balon hijau\nHatiku sangat kacau\nBalonku tinggal 4\nKupegang erat-erat",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey[700],
+                fontFamily: 'Arial',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
